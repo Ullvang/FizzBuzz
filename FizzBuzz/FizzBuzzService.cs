c751af5338ad20e20d041a13ae4305a51e67d59b
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-01-21 22:37:01 +0100

    added IFizzBuzzService, and made FizzBuzzService class implement the interface

---
+++ FizzBuzz/FizzBuzzService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public interface IFizzBuzzService
+    {
+        string[] GetFizzBuzz(uint number);
+    }
+
+    class FizzBuzzService : IFizzBuzzService
+    {
+        public string[] GetFizzBuzz(uint number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
