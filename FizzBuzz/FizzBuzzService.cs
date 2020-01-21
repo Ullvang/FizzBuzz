9fade9a25da64002ae60d27f0162e141040634dc
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-01-21 22:38:25 +0100

    updated GetFizzBuzz method

--- FizzBuzz/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzzService.cs
@@ -13,7 +13,27 @@ namespace FizzBuzz
     {
         public string[] GetFizzBuzz(uint number)
         {
-            throw new NotImplementedException();
+            string[] result = new string[number];
+            for (int i = 0; i < number; i++)
+            {
+                if ((i + 1) % 15 == 0)
+                {
+                    result[i] = "FizzBuzz";
+                }
+                else if ((i + 1) % 5 == 0)
+                {
+                    result[i] = "Buzz";
+                }
+                else if ((i + 1) % 3 == 0)
+                {
+                    result[i] = "Fizz";
+                }
+                else
+                {
+                    result[i] = (i + 1).ToString();
+                }
+            }
+            return result;
         }
     }
 }
