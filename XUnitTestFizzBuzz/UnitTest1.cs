f848650d74ebd8a71909b1040f79391639a327e1
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-01-21 22:31:33 +0100

    Unit test worked, deleted fake method implimitation

--- XUnitTestFizzBuzz/UnitTest1.cs
+++ XUnitTestFizzBuzz/UnitTest1.cs
@@ -5,26 +5,6 @@ namespace XUnitTestFizzBuzz
 {
     public class UnitTest1
     {
-        //Fake method
-        static string GetFizzBuzz(uint num)
-        {
-            switch (num)
-            {
-                case 1:
-                    return "1";
-                case 3:
-                    return "Fizz";
-                case 5:
-                    return "Buzz";
-                case 13:
-                    return "13";
-                case 15:
-                    return "FizzBuzz";
-                default:
-                    return "";
-            }
-        }
-
         [Theory]
         [InlineData(1, "1")]
         [InlineData(3, "Fizz")]
